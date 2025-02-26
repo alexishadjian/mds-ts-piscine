0dba64efc947c787617c9e17cf8baf63400c49b9
Alexis <90972106+alexishadjian@users.noreply.github.com>
2025-02-26 11:55:51 +0100

    feat: s3 ep2

---
+++ src/s3/ep2/formes.ts
@@ -0,0 +1,42 @@
+class Rectangle {
+    largeur: number;
+    hauteur: number;
+
+    constructor(largeur: number, hauteur: number) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+
+    calculerSurface(): number {
+        return this.largeur * this.hauteur;
+    }
+}
+
+class Cercle {
+    rayon: number;
+
+    constructor(rayon: number) {
+        this.rayon = rayon;
+    }
+
+    calculerSurface(): number {
+        return Math.PI * this.rayon * this.rayon;
+    }
+}
+
+
+function afficherSurface(forme: Rectangle | Cercle): void {
+    if (forme instanceof Rectangle) {
+        console.log(`Ceci est un rectangle de surface ${forme.calculerSurface()}`);
+    } else {
+        console.log(`Ceci est un cercle de surface ${forme.calculerSurface()}`);
+    }
+}
+
+
+const rectangle = new Rectangle(10, 20);
+const cercle = new Cercle(5);
+
+
+console.log(rectangle.calculerSurface());
+console.log(cercle.calculerSurface());
