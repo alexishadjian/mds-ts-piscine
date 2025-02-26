541be2d4388be37c2341d63cd458b31787a07ea2
Alexis <90972106+alexishadjian@users.noreply.github.com>
2025-02-26 10:04:26 +0100

    feat: ep1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,3 @@
-console.log('Hello, TypeScript!');
+console.log('[Hello, TypeScript!]');
 
 document.querySelector("h3")!.textContent = "Hello, TypeScript!";
---
+++ src/personne.ts
@@ -0,0 +1,16 @@
+interface Personne {
+    nom: string;
+    age: number;
+}
+
+function afficherPersonne(personne: Personne): void {
+    console.log(`Bonjour, je m'appelle ${personne.nom} et j'ai ${personne.age} ans.`);
+}
+
+const personne1 = {
+    nom: 'Alice',
+    age: 25
+}
+
+
+afficherPersonne(personne1);
